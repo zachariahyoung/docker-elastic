e8ddf8f5f732332c3bb209fee10df3d2810edf1f
Zachariah Young <zpyoung@gmail.com>
2016-01-24 17:52:18 -0600

    update route need to match server and client

--- src/main/java/com/zandriod/elastic/ElasticApplication.java
+++ src/main/java/com/zandriod/elastic/ElasticApplication.java
@@ -9,8 +9,8 @@ public class ElasticApplication extends FatJarRouter {
 	@Override
 	public void configure() throws Exception {
 
-				from("weather:foo?appid=44db6a862fbab1930da0d769e98").routeId("weather")
-				.to("elasticsearch://weather-indexer?operation=INDEX&indexName=weather&indexType=72764");
+				from("weather:foo?appid=44db6a862fba0b067b1930da0d769e9").routeId("weather")
+				.to("elasticsearch://weather-indexer?operation=INDEX&indexName=weather&indexType=72764&transportAddresses=localhost:9300");
 
 	}
 }
